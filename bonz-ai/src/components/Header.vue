7abb5de68480ed72673909cd65fae5dc51d50500
BacHaddad <br.yhaddad@gmail.com>
2021-11-13 22:21:38 +0100

    create a header component

---
+++ bonz-ai/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <nav class="header_navbar">
+      <router-link to="/"> Bonz.AI </router-link>
+    </nav>
+
+    <article class="header_text">
+      <h1>Bonz.AI</h1>
+      <h2>An AI-Powered Eco-Stay</h2>
+      <aside></aside>
+    </article>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('../assets/header-image.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 110% 110%;
+
+  &_text {
+    position: absolute;
+    top: 13.875rem;
+    left: 10.438rem;
+  }
+}
+</style>
--- bonz-ai/src/views/Home.vue
+++ bonz-ai/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <Navbar msg="Welcome to Your Vue.js App" />
-  </div>
+  <section>
+    <Header />
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/Navbar.vue';
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'Home',
   components: {
-    Navbar,
+    Header,
   },
 };
 </script>
